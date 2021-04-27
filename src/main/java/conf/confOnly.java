db61349bbcabf806ef79b41820dbbec4d3859bd4
Vladimirov Stanislav <zstanislaw211@gmail.com>
2021-04-27 13:31:36 +0300

    first commit

---
+++ src/main/java/conf/confOnly.java
@@ -0,0 +1,29 @@
+package conf;
+
+public class confOnly {
+/*
+ 
+ #Git
+ 
+ git pull
+  
+ cd C:/Projects2/FavoritAvto
+ git add . 
+ git reset -- 'main/resources/*'
+
+ 
+ 
+ git commit -m  "Done J2SE 6" 
+ git push -u origin master
+ 
+ 
+ #maven
+ cd C:/Projects2/FavoritAvto
+ mvn clean compile assembly:single
+ 
+
+ java -jar C:\vianor_stock\ru.badyavina.www-0.0.1-SNAPSHOT-jar-with-dependencies.jar "C:\vianor_stock" "C:\vianor_stock" "Products_Vianor_589_32202gg.csv" "C:\vianor_stock\CentralProvider" "C:\vianor_stock\OtherProvider" "Stanislav.Vladimirov@nokiantyres.com" "zstanislaw211@gmail.com"
+ 
+ 
+ */
+}
